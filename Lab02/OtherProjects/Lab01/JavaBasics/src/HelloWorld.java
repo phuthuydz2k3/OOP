e524aa7a02014be62a094dfdb52f59a28470b0aa
Tran Anh <concacdick@gmail.com>
2023-05-15 19:10:11 +0700

    new committ

--- Lab02/OtherProjects/Lab01/JavaBasics/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        System.out.println("TranAnh - 20215179");
-        System.out.println("Hello \n Everyone!");
-        System.out.println("Hello \t World!");
-    }
-}
